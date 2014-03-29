163bff22d83fc0761b28b6aa7020658a959c2aef
Daniel Beasley <danbzly@gmail.com>
2014-03-28 20:04:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wodonomics::Application.config.secret_key_base = 'fa3b2e207f1f5d96ecd2042924359fafc783e69733a8fe8611ed0df34a5ff42eedf9e77d8614fc131c8188a278d6442745003ad170b11176ae528c8b5823f432'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wodonomics::Application.config.session_store :cookie_store, key: '_wodonomics_session'
