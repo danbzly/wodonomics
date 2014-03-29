55e2098749f8f4c0debacc7645d53aa5113951b6
Daniel Beasley <danbzly@gmail.com>
2014-03-29 02:30:57 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-config.assets.initialize_on_precompile = false
+
 
 module Wodonomics
   class Application < Rails::Application
